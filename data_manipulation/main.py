6371e67c8ac339b8323bdcac0791a86f79df6a2d
Vashchonak Anastasiya <70859134+nastyavasch@users.noreply.github.com>
2022-08-31 18:46:03 +0300

    fixed 1st and added 2nd conditions

--- data_manipulation/main.py
+++ data_manipulation/main.py
@@ -1,32 +1,23 @@
 import pandas as pd
-from datetime import datetime
+from datetime import datetime 
 
-colnames = 'error_code', 'error_message', 'severity', 'log_location', 'mode', 'model', 'graphics', 'session_id', 'sdkv', 'test_mode',
-'flow_id', 'flow_type', 'sdk_date', 'publisher_id', 'game_id', 'bundle_id', 'appv', 'language', 'os', 'adv_id', 'gdpr', 'ccpa', 'country_code', 'date'
+colnames = 'error_code', 'error_message', 'severity', 'log_location', 'mode', 'model', 'graphics', 'session_id', 'sdkv', 'test_mode', 'flow_id', 'flow_type', 'sdk_date', 'publisher_id', 'game_id', 'bundle_id', 'appv', 'language', 'os', 'adv_id', 'gdpr', 'ccpa', 'country_code', 'date'
 
 df = pd.read_csv('data/data.csv', names = colnames, header = None, engine = 'python', skiprows = 1)
-df['date'] = pd.to_datetime(df['date'], errors = 'coerce')
-
-df['date'] = df['date'].values.astype(float)
-
-sec = 1000000000 #one second in ns
-minute = sec * 60 #one minute
-hour = minute * 60 #one hour
-
-error_counter = 0
-dt_counter = 0
+df['date'] = pd.to_datetime(df['date'], errors = 'coerce', unit = 's')
+df_errors = df[df.severity == 'Error']
 
 #condition 1
-for i in range(len(df.index)):
-    if df['severity'].iloc[i] == 'Error':
-        error_counter += 1
-    if (error_counter > 10) and (dt_counter < minute):
-        print(f"ALERT! {error_counter} errors in {dt_counter/sec} seconds") 
-        error_counter = 0
-        dt_counter = 0 
-    dt_counter += df['date'].iloc[i] 
-    continue
+def error_logs(df):
+    error_num = df_errors.groupby(pd.Grouper(key='date', freq='1Min', dropna=True))['severity'].count()
+    result = error_num[error_num > 10]
+    return result
+
 #condition 2
+def error_logs_for_bundle(df):
+    error_num = df_errors.groupby([pd.Grouper(key='date', freq='60Min', dropna=True),'bundle_id'])['severity'].value_counts()
+    result = error_num[error_num > 10]
+    return result
 
-
-print('test')
+print(error_logs(df))
+print(error_logs_for_bundle(df))
