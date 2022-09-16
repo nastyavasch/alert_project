d91b896b50f0b1fb30828c7722e6a372e343f5ed
Vashchonak Anastasiya <70859134+nastyavasch@users.noreply.github.com>
2022-09-16 16:28:36 +0300

    added logging

--- data_manipulation/main.py
+++ data_manipulation/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import logging as lg
 
 colnames = ['error_code', 'error_message', 'severity', 'log_location', 'mode', 'model', 'graphics', 'session_id', 'sdkv', 'test_mode', 'flow_id', 'flow_type', 'sdk_date', 'publisher_id', 'game_id', 'bundle_id', 'appv', 'language', 'os', 'adv_id', 'gdpr', 'ccpa', 'country_code', 'date']
 
@@ -15,5 +16,5 @@ def error_logs(df, freq, col= 'severity'):
 # freq - time frequency
 # col - columns we want to add to group by. default = 'severity' column
 
-print(error_logs(df,'1Min')) #condition 1
-print(error_logs(df,'60Min', 'bundle_id')) #condition 2
+lg.error(error_logs(df,'1Min')) #condition 1
+lg.error(error_logs(df,'60Min', 'bundle_id')) #condition 2
