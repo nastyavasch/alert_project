86575c93d7dd5e68a8bb0e9e4934f74301f831de
Vashchonak Anastasiya <70859134+nastyavasch@users.noreply.github.com>
2022-09-16 16:20:06 +0300

    added poetry to dockerfile and rewrited main func

--- data_manipulation/main.py
+++ data_manipulation/main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from datetime import datetime 
 
 colnames = ['error_code', 'error_message', 'severity', 'log_location', 'mode', 'model', 'graphics', 'session_id', 'sdkv', 'test_mode', 'flow_id', 'flow_type', 'sdk_date', 'publisher_id', 'game_id', 'bundle_id', 'appv', 'language', 'os', 'adv_id', 'gdpr', 'ccpa', 'country_code', 'date']
 
@@ -8,16 +7,13 @@ df['date'] = pd.to_datetime(df['date'], errors = 'coerce', unit = 's')
 df_errors = df[df.severity == 'Error']
 
 #condition 1
-def error_logs(df):
-    error_num = df_errors.groupby(pd.Grouper(key='date', freq='1Min', dropna=True))['severity'].count()
+def error_logs(df, freq, col= 'severity'):
+    df_errors = df[df.severity == 'Error']
+    error_num = df_errors.groupby([pd.Grouper(key='date', freq=freq, dropna=True)] + [col])['severity'].count()
     result = error_num[error_num > 10]
     return result
+# freq - time frequency
+# col - columns we want to add to group by. default = 'severity' column
 
-#condition 2
-def error_logs_for_bundle(df):
-    error_num = df_errors.groupby([pd.Grouper(key='date', freq='60Min', dropna=True),'bundle_id'])['severity'].value_counts()
-    result = error_num[error_num > 10]
-    return result
-
-print(error_logs(df))
-print(error_logs_for_bundle(df))
+print(error_logs(df,'1Min')) #condition 1
+print(error_logs(df,'60Min', 'bundle_id')) #condition 2
